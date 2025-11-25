7383ccd933e8eded80ccc561c6389d38768394f7
zhaoyuyoung <zoe.anhui@gmail.com>
2025-11-25 14:15:51 -0800

    Adding prints to check config

--- python/lsst/resources/davutils.py
+++ python/lsst/resources/davutils.py
@@ -1069,6 +1069,9 @@ class DavClient:
             "Content-Length": str(len(body)),
         }
         print(f"Testing....... DavClient PROPFIND url:{url}")
+        print("Backend cert_file:", self._backend.connection_from_url(url).cert_file)
+        print("Backend key_file:", self._backend.connection_from_url(url).key_file)
+        print("Backend ca_cert_dir:", self._backend.connection_from_url(url).ca_cert_dir)
         resp = self._request("PROPFIND", url=url, headers=headers, body=body, pool_manager=self._backend)
         if resp.status in (HTTPStatus.MULTI_STATUS, HTTPStatus.NOT_FOUND):
             return resp
